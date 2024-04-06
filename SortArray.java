b9df9e9568d5f970a563a65c93792a8aae5c9b7c
Mankirat Singh <kiratfatehgarh@gmail.com>
2024-04-06 16:38:38 -0700

    Adding a  inline comment

--- SortArray.java
+++ SortArray.java
@@ -6,6 +6,7 @@ public class SortArray {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
+        // Adding a new inline comment for the demonstration
         // Prompt user to enter the number of integers
         System.out.print("Enter the number of integers: ");
         int numIntegers = scanner.nextInt();
